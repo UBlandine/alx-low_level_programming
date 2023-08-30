340c29bf572e1fa92664fcf007de296f1c48a3a6
UBlandine <ugirablando@gmail.com>
2023-08-30 03:13:08 -0700

    Why is it so important to dream? Because, in my dreams we are together

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - write the character c to stdout
+ * @c: The char to print
+ * Return: on success 1
+ * On error, -1 returned
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
